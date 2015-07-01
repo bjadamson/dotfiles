712eeee3fad704fe30fadf9f5276b7a2e2ef6f0e
Benjamin Adamson <badamson@amazon.com>
2015-06-30 19:51:17 -0700

    Remove unneeded echo statements

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,5 @@ set TERM xterm-256color
 # Unset the path, we'll build it all by ourselves!
 #set -e PATH
 
-echo $PATH
 . ~/.config/fish/path.fish
-echo $PATH
 . ~/.config/fish/work/top-level.fish
-echo $PATH
