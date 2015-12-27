50d03fea8b8549adae1bccfd05c329cf5df6cb74
Benjamin Adamson <adamson.benjamin@gmail.com>
2015-12-27 21:29:10 +0000

    nvim -> vim

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -x PATH /usr/local/bin /bin /usr/sbin /sbin /usr/local/git/bin $PATH /opt/ghc/7.10.2/bin/
 set TERM xterm-256color
-git config --global core.editor "nvim"
+git config --global core.editor "vim"
 
 # Unset the path, we'll build it all by ourselves!
 #set -e PATH
