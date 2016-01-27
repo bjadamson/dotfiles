ca3ddc7c6ec2c6a4c7e7fabb6759b4e240a430c4
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-01-26 23:51:58 -0800

    Beginning to figure out work only stuff.

--- setup/work.py
+++ setup/work.py
@@ -2,25 +2,20 @@
 import socket
 import fileinput
 
-def _add_amazon_specific_fish_config_file(homedir):
-    print "Adding amazon specific fish config file."
-    with open(homedir + 'fish/config.fish', 'a+') as f:
-        f.write('. ~/.config/fish/work/top-level.fish\n')
-
 def _add_amazon_specific_zsh_file(homedir):
-    print "Adding amazon only zsh config"
+    print "Adding amazon only zsh config, homedir {0}".format(homedir)
     with open(homedir + '.zshrc', 'a') as f:
-        f.write('source $PWD_DOTFILES/amazon')
+        f.write('source $ZSH_DOTFILES/amazon')
 
 def _replace_git_alias(prefix):
     print 'Replacing git alias, this is a work image.'
-    dst = prefix + '/.gitconfig'
+    dst = prefix + '.gitconfig'
     for line in fileinput.input(dst, inplace=True):
         print line.replace(r'adamson.benjamin@gmail.com', 'badamson@amazon.com'),
 
 def initialize_for_amazon_host(prefix):
     _replace_git_alias(prefix)
-    _add_amazon_specific_fish_config_file(prefix)
+    _add_amazon_specific_zsh_file(prefix)
 
 def is_amazon_hostname(homedir):
     return '.amazon.com' in socket.gethostname()
