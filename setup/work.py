62f86eb26c0f008c543b4d28a7efce1633eb68d4
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-01-27 00:27:34 -0800

    Initial work zsh templates (not tested)

--- setup/work.py
+++ setup/work.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python2.7
-import socket
 import fileinput
+import socket
 
-def _add_amazon_specific_zsh_file(homedir):
+def _add_amazon_specific_zsh_file(homedir, dotfiles_directory):
     print "Adding amazon only zsh config, homedir {0}".format(homedir)
-    with open(homedir + '.zshrc', 'a') as f:
+    with open(dotfiles_directory + '/zshrc', 'a') as f:
         f.write('source $ZSH_DOTFILES/amazon')
 
-def _replace_git_alias(prefix):
+def _replace_git_alias(homedir):
     print 'Replacing git alias, this is a work image.'
-    dst = prefix + '.gitconfig'
+    dst = homedir + '/.gitconfig'
     for line in fileinput.input(dst, inplace=True):
         print line.replace(r'adamson.benjamin@gmail.com', 'badamson@amazon.com'),
 
-def initialize_for_amazon_host(prefix):
-    _replace_git_alias(prefix)
-    _add_amazon_specific_zsh_file(prefix)
+def initialize_for_amazon_host(homedir, dotfiles_directory):
+    _replace_git_alias(homedir)
+    _add_amazon_specific_zsh_file(homedir, dotfiles_directory)
 
 def is_amazon_hostname(homedir):
     return '.amazon.com' in socket.gethostname()
