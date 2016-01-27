8fa8d0de0345a748c782ad78a17c73f8bf718e18
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-01-26 23:21:21 -0800

    fix zsh alias

--- setup/install.py
+++ setup/install.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python2.7
 import os
 
-def symlink_thing(prefix, homedir, dotfile, target):
-    dst = homedir + '/' + dotfile
-    if os.path.islink(dst):
-        val = os.unlink(dst)
-    val = os.symlink(prefix + target, dst)
-    print "val '" + str(val) + "'"
-
 def system_dependency(name):
     print "installing system dependency {}".format(name)
     os.system('sudo apt-get install %s' % name)
