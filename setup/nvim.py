34159f26bf608000bd9264cf257965edde9e040c
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-05-27 22:48:47 -0700

    more nvim -> vim,  configure now works again

--- setup/nvim.py
+++ setup/nvim.py
@@ -2,7 +2,7 @@
 import os, shutil
 
 def clone_vundle(homedir):
-    dst = homedir + '/.vim/bundle/Vundle.vim'
+    dst = homedir + '/.nvim/bundle/Vundle.vim'
     if os.path.exists(dst):
         shutil.rmtree(dst)
     os.system('git clone https://github.com/gmarik/Vundle.vim.git ' + dst)
