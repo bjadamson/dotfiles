c6530355297b40aa3bc79930a583968b52018579
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-06-03 17:29:02 -0700

    Add clang alias's.

--- setup/install.py
+++ setup/install.py
@@ -5,3 +5,6 @@ def system_dependency(name):
     print "installing system dependency {}".format(name)
     os.system('sudo apt-get install %s' % name)
     print "done!"
+
+def add_repo(url):
+    os.system("sudo apt-add-repository {}".format(url))
